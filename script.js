2d023e8e9a2d1cb0656f8cf2319043747d39db3a
Mohammad Hoseinpour <78277433+hpourv@users.noreply.github.com>
2023-08-24 20:52:36 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const ball = document.querySelector('.ball');
+const scatterPlotCanvas = document.getElementById('scatterPlot');
+const ctx = scatterPlotCanvas.getContext('2d');
+
+const positionData = []; // Array to store position data for scatter plot
 
 function getRandomPosition(min, max) {
   return Math.random() * (max - min) + min;
@@ -13,7 +17,43 @@ function moveBallRandomly() {
   
   ball.style.left = `${randomX}px`;
   ball.style.top = `${randomY}px`;
+  
+  // Add current position to positionData array
+  positionData.push({ x: randomX, y: randomY });
+  
+  updateScatterPlot();
 }
 
-// Move the ball randomly every 1 second
+function updateScatterPlot() {
+  const scatterChart = new Chart(ctx, {
+    type: 'scatter',
+    data: {
+      datasets: [{
+        label: 'Ball Position',
+        data: positionData,
+        borderColor: 'rgba(255, 99, 132, 1)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        pointRadius: 5,
+        pointHoverRadius: 8,
+        showLine: false
+      }]
+    },
+    options: {
+      scales: {
+        x: {
+          type: 'linear',
+          position: 'bottom'
+        },
+        y: {
+          min: 0,
+          max: window.innerHeight,
+          type: 'linear',
+          position: 'left'
+        }
+      }
+    }
+  });
+}
+
+// Move the ball randomly every 100 milliseconds
 setInterval(moveBallRandomly, 100);
